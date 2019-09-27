9fbed0580e8be68741e71b2acbfeac0c74b0bdbc
ahmetemrekilinc <aekilinc@gmail.com>
2019-09-27 14:37:47 +0300

    interface fix

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false
+    }
     // Add options here
   });
 
